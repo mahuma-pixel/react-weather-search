95e4500f9f01895bce71b8bd2fd250b88a9588cb
mahuma-pixel <69123574+mahuma-pixel@users.noreply.github.com>
2024-10-17 23:39:57 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 function App() {
     return (
       <div className="App">
-        <h2> Weather App</h2>
         <footer>
         This project was coded by <a href="mailto:mahoomah.curl@gmail.com" target='_blank' rel="noreferrer"> Ribane M</a> and is open-sourced on <a href="https://github.com/mahuma-pixel" target='_blank' rel="noreferrer">GitHub</a> and hosted on <a href="https://react-weather-homework-mahuma.netlify.app/" target='_blank' rel="noreferrer">Netlify</a>
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -3,16 +3,30 @@ import React from "react";
 export default function Weather(){
 
     return <div className="weather">
-        <form>
-            <input type="Search" placeholder="Enter city"/>
-            <input type="Submit" value={"Search"}/>
-        </form>
-        <h2>
+        <div className="Container">
+            <div className="row">
+                <div className=""><form>
+            <input type="Search" placeholder="Enter city" className="form col-9"/>
+            <input type="Submit" value={"Search"} className=" col-3 btn btn-primary w-20 "/>
+        </form></div></div></div>
+        <h1>
             Paris
-        </h2>
+        </h1>
         <ul>
             <li>Wednesday 11h00</li>
             <li>Mostly cloudy</li>
         </ul>
+        <div className="container">
+            <div className="row">
+                <div className="col-6"> <img src="images\download.png" alt="forecast"></img> 6 <span>°C</span></div>
+                <div className="col-6">
+                <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 5km/h%</li>
+        </ul>
+                </div>
+            </div>
+        </div>
     </div>
 }
