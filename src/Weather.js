d9a2561254baf6f8f846286314f54de386fc73bc
mahuma-pixel <69123574+mahuma-pixel@users.noreply.github.com>
2024-10-17 22:40:12 +0200

    added weather.css

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Weather(){
+
+    return <div>
+        <form>
+            <input type="Search" placeholder="Enter city"/>
+            <input type="Submit" value={"Search"}/>
+        </form>
+    </div>
+}
